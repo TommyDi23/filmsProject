6b4423467f417350c1bc1ab28949a37053d1b0d2
Tommy Diep <tommydi23@Tommys-Air.broadband>
2020-03-26 18:58:18 +0000

    testing psql

---
+++ db/data/test-data/comments.js
@@ -0,0 +1,44 @@
+module.exports = [
+  {
+    body: "Invisible man was terrible avoid at all cost, thank me later",
+    belongs_to: "Invisible Man",
+    created_by: "Kirsty M",
+    votes: 16,
+    created_at: 1511354163389
+  },
+  {
+    body: "great movie would watch over and over again",
+    belongs_to: "Parasite",
+    created_by: "Kirsty M",
+    votes: 14,
+    created_at: 1479818163389
+  },
+  {
+    body: "best movie this year",
+    belongs_to: "Parasite",
+    created_by: "Tommy Di",
+    votes: 100,
+    created_at: 1448282163389
+  },
+  {
+    body: "went to watch this with friends we really enjoyed it",
+    belongs_to: "Parasite",
+    created_by: "Tommy Di",
+    votes: -100,
+    created_at: 1416746163389
+  },
+  {
+    body: "must see, would recommend",
+    belongs_to: "Parasite",
+    created_by: "Tommy Di",
+    votes: 0,
+    created_at: 1385210163389
+  },
+  {
+    body: "It was ok to watch but not the best",
+    belongs_to: "Parasite",
+    created_by: "Tommy Di",
+    votes: 0,
+    created_at: 1353674163389
+  }
+];
---
+++ db/setup.sql
@@ -0,0 +1,5 @@
+DROP DATABASE IF EXISTS project_films_test;
+DROP DATABASE IF EXISTS project_films;
+
+CREATE DATABASE project_films_test;
+CREATE DATABASE project_films;
