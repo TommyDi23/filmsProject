c5a8d349393b31f15ed7fbbbf26cd009759d0758
Kirsty McGlynn <kirsty.lmcg@gmail.com>
2020-03-27 14:17:38 +0000

    Migration setup

---
+++ db/data/test-data/users.js
@@ -0,0 +1,18 @@
+module.exports = [
+  {
+    username: "KLM94",
+    name: "Kirsty"
+  },
+  {
+    username: "TommoD",
+    name: "Tommy"
+  },
+  {
+    username: "Batman",
+    name: "Bruce"
+  },
+  {
+    username: "lurker",
+    name: "do_nothing"
+  }
+];
---
+++ db/migrations/20200327132241_create_table_comments.js
@@ -0,0 +1,18 @@
+exports.up = function(knex) {
+  return knex.schema.createTable("comments", function(table) {
+    table.increments("comment_id").primary();
+    table
+      .string("author")
+      //   .references("users.username")
+      .notNullable();
+    table.integer("film_id");
+    // .references("films.film_id");
+    table.integer("votes").defaultTo(0);
+    table.timestamp("created_at").defaultTo(knex.fn.now());
+    table.text("body").notNullable();
+  });
+};
+
+exports.down = function(knex) {
+  return knex.schema(dropTable("comments"));
+};
