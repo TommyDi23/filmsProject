f1983141a780748e9273c93ae5e3fb3188a0653b
Tommy Diep <tommydi23@Tommys-Air.broadband>
2020-03-29 16:13:18 +0100

    utils testing done

--- spec/utils.spec.js
+++ spec/utils.spec.js
@@ -6,8 +6,7 @@ describe("formatDates", () => {
     expect(formatDates([])).to.eql([]);
   });
 
-  
-  it("returns an array with created_at time adjusted", () => {
+  it("returns an array with one object with created_at time adjusted", () => {
     const input = [
       {
         body: "Invisible man was terrible avoid at all cost, thank me later",
@@ -28,5 +27,64 @@ describe("formatDates", () => {
       }
     ];
     expect(actual).to.eql(expected);
+  });
+  it("returns an array of multiple objects with created_at time adjusted in each object", () => {
+    const input = [
+      {
+        body: "Invisible man was terrible avoid at all cost, thank me later",
+        film_id: 1,
+        author: "KLM94",
+        votes: 16,
+        created_at: 1511354163389
+      },
+      {
+        body: "great movie would watch over and over again",
+        film_id: 2,
+        author: "KLM94",
+        votes: 14,
+        created_at: 1479818163389
+      }
+    ];
+    const actual = formatDates(input);
+    const expected = [
+      {
+        body: "Invisible man was terrible avoid at all cost, thank me later",
+        film_id: 1,
+        author: "KLM94",
+        votes: 16,
+        created_at: new Date(1511354163389)
+      },
+      {
+        body: "great movie would watch over and over again",
+        film_id: 2,
+        author: "KLM94",
+        votes: 14,
+        created_at: new Date(1479818163389)
+      }
+    ];
+    expect(actual).to.eql(expected);
+  });
+  it("check for mutation to original array", () => {
+    const input = [
+      {
+        body: "Invisible man was terrible avoid at all cost, thank me later",
+        film_id: 1,
+        author: "KLM94",
+        votes: 16,
+        created_at: 1511354163389
+      }
+    ];
+    const actual = formatDates(input);
+    const expected = [
+      {
+        body: "Invisible man was terrible avoid at all cost, thank me later",
+        film_id: 1,
+        author: "KLM94",
+        votes: 16,
+        created_at: new Date(1511354163389)
+      }
+    ];
+    expect(actual).to.eql(expected);
+    expect(input).to.not.eql(expected);
   });
 });
